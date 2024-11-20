464f245833856c165672d9b9ed21b47d9c76f086
Prashant956148 <prashantkarande5055@gmail.com>
2024-11-20 16:47:47 +0530

    Delete todo-data-boot directory

--- todo-data-boot/src/main/java/com/zs/repo/userrepo.java
+++
@@ -1,9 +0,0 @@
-package com.zs.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.zs.entity.User;
-
-public interface userrepo extends JpaRepository<User, Integer> {
-
-}
